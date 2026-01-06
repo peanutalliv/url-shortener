634e92503356199da388e6f46e627ee023fcd048
Robin Villa <robinv89@gmail.com>
2026-01-06 01:05:22 -0500

    Small edit to index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // optional, can create an empty file
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
